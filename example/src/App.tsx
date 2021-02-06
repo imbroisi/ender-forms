1fd71b1ab77ef55ff25d72dbc1db0bf9148de727
Luiz Imbroisi <luizimbroisi@gmail.com>
2021-02-06 14:49:57 -0300

    init create-react-library@3.1.1

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { ExampleComponent } from 'ender-forms'
+import 'ender-forms/dist/index.css'
+
+const App = () => {
+  return <ExampleComponent text="Create React Library Example 😄" />
+}
+
+export default App
